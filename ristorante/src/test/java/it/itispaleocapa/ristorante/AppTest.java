73bb86bba0d10fb2787e001f9953459e429793f5
silviaarnoldi <101811166+silviaarnoldi@users.noreply.github.com>
2023-05-22 16:09:42 +0000

    c

--- ristorante/src/test/java/it/itispaleocapa/ristorante/AppTest.java
+++ ristorante/src/test/java/it/itispaleocapa/ristorante/AppTest.java
@@ -1,18 +1,13 @@
 package it.itispaleocapa.ristorante;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import java.time.LocalDate;
+import java.util.LinkedList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
  */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
-    @Test
-    void testApp() {
-        assertEquals(1, 1);
-    }
+    
 }
